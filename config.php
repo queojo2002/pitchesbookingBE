fce385fe2482fcda3e26cb288ba33fde63ab04ed
quejo_1101 <ducln339@gmail.com>
2024-06-04 15:06:28 +0700

    update

--- config.php
+++ config.php
@@ -11,7 +11,7 @@ date_default_timezone_set('Asia/Ho_Chi_Minh');
 $vnp_TmnCode = "6LY4D08E"; //Mã định danh merchant kết nối (Terminal Id)
 $vnp_HashSecret = "L32MNN2JMUFDPMQZXE1IQB3QFKV75MI3"; //Secret key
 $vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-$vnp_Returnurl = "http://192.168.1.56/pitchesbookingBE/vnpay_return.php";
+$vnp_Returnurl = "https://vanduc.top/pitchesBookingApi/vnpay_return.php";
 $vnp_apiUrl = "http://sandbox.vnpayment.vn/merchant_webapi/merchant.html";
 $apiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
 //Config input format
